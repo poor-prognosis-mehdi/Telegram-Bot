523497cb7906a866539a95c690412f57577dc430
Mohammad mehdi Daghighian <mohammadmehdidaghighian@gmail.com>
2026-09-02 11:28:50 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import telebot
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-TOKEN ="7767860852:AAG19eWI27LLYDcWcAhS9JGusom2IQMjZ1w"
+TOKEN ="7767860852:AAHq5WqZ5ETffXQY671t9Adxkf_-33qu_6o"
 ADMIN_ID = 218104646
 
 ABOUT_ME_TEXT = """سلام دوست عزیز 👋
